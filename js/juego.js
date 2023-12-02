cb2368438c60bb1bb42659efc8d17f1f7039b8d2
Roberto <roberto.v.arebalo@gmail.com>
2023-12-01 21:55:46 -0300

    volviendo a comenzar

--- js/juego.js
+++ js/juego.js
@@ -1,2 +1,14 @@
-const btnVerMas = document.getElementById("startBtn");
-console.log(btnVerMas);
+
+
+
+function flyToSun() {
+    var guessNumber = document.getElementById('guessNumber').value;
+    var paperPlane = document.createElement('div');
+    paperPlane.style.position = 'absolute';
+    paperPlane.style.width = '50px';
+    paperPlane.style.height = '50px';
+    paperPlane.style.backgroundImage = "url('paper_plane.png')";
+    paperPlane.style.backgroundSize = 'cover';
+    document.body.appendChild(paperPlane);
+    // Aquí puedes agregar la animación para que el avión de papel vuele hacia el sol.
+}
