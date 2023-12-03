f5bcb0ff0eae4f6d6a17e8b684418a94321488ba
Roberto <roberto.v.arebalo@gmail.com>
2023-12-03 16:03:39 -0300

    Agrego parte de la logica

--- js/juego.js
+++ js/juego.js
@@ -8,13 +8,17 @@ function numeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 function comprobarNumero(numero) {
-    if (parseInt(numero) === numeroMagico ) {
-        alert(`Feliciades!, tienes una gran intuicion! el numero era: ${numeroMagico}`)
-    } else if(parseInt(numero) > numeroMagico){
-        alert(`Tu numero es algo mas grande que el numero magico...`)
-    } else{
-        alert(`Tu numero es menor que el numero magico...`)
-    }
+  if (parseInt(numero) === numeroMagico) {
+    alert(
+      `Feliciades!, tienes una gran intuicion! el numero era: ${numeroMagico}`
+    );
+  } else if (parseInt(numero) > numeroMagico) {
+    alert(`Tu numero es algo mas grande que el numero magico...`);
+  } else if (numero === "") {
+    alert(`Pero... emm... no ingresaste ningun numero :/`);
+  } else{
+    alert(`Tu numero es menor que el numero magico...`);
+  }
 }
 
 let numeroMagico = numeroAleatorio(0, 100);
