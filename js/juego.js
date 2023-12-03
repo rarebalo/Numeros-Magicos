066a76c8b488bb371054f36573370201d49d9a3e
Roberto <roberto.v.arebalo@gmail.com>
2023-12-03 15:38:49 -0300

    Agrego la logica

--- js/juego.js
+++ js/juego.js
@@ -1,14 +1,20 @@
-
-
-
-function flyToSun() {
-    var guessNumber = document.getElementById('guessNumber').value;
-    var paperPlane = document.createElement('div');
-    paperPlane.style.position = 'absolute';
-    paperPlane.style.width = '50px';
-    paperPlane.style.height = '50px';
-    paperPlane.style.backgroundImage = "url('paper_plane.png')";
-    paperPlane.style.backgroundSize = 'cover';
-    document.body.appendChild(paperPlane);
-    // Aquí puedes agregar la animación para que el avión de papel vuele hacia el sol.
+const tomarTexto = () => {
+  const numeroDelUsuario = document.getElementById("textoPrediccion");
+  comprobarNumero(numeroDelUsuario.value);
+};
+function numeroAleatorio(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 }
+function comprobarNumero(numero) {
+    if (parseInt(numero) === numeroMagico ) {
+        alert(`Feliciades!, tienes una gran intuicion! el numero era: ${numeroMagico}`)
+    } else if(parseInt(numero) > numeroMagico){
+        alert(`Tu numero es algo mas grande que el numero magico...`)
+    } else{
+        alert(`Tu numero es menor que el numero magico...`)
+    }
+}
+
+let numeroMagico = numeroAleatorio(0, 100);
